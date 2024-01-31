a8dc93727f916147b8c35608a987d1eefdd3ebac
LeiDengIDS <138241434+LeiDengIDS@users.noreply.github.com>
2024-01-31 12:03:36 +0100

    Update new_ml_server.py
    Signed-off-by: LeiDengIDS <138241434+LeiDengIDS@users.noreply.github.com>

--- data_extractor/code/new_ml_based_pipeline/new_ml_based_pipeline/new_ml_server.py
+++ data_extractor/code/new_ml_based_pipeline/new_ml_based_pipeline/new_ml_server.py
@@ -1,37 +1,107 @@
 import argparse
 import os
+import json
 
 from flask import Flask, Response, request
+from s3_communication import S3Communication
 
 app = Flask(__name__)
 
-def run_new_ml_int(raw_pdf_folder, working_folder, output_folder, verbosity):
-        cmd = 'python3 /app/code/new_ml_based_pipeline/new_ml_based_pipeline/main.py' + \
-              ' --raw_pdf_folder "' + raw_pdf_folder + '"' +    \
-              ' --working_folder "' + working_folder + '"' +    \
-              ' --output_folder "' + output_folder + '"'  +     \
-              ' --verbosity ' + str(verbosity)
-        print("Running command: " + cmd)
-        os.system(cmd)
+def create_directory(directory_name):
+    os.makedirs(directory_name, exist_ok=True)
+    for filename in os.listdir(directory_name):
+        file_path = os.path.join(directory_name, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
 
-def run_new_ml(project_name, verbosity):
-    base = r'/app/data/' + project_name 
-    raw_pdf_folder = base + r'/interim/pdfs/'
-    working_folder = base + r'/interim/rb/work'
-    output_folder = base + r'/output/KPI_EXTRACTION/rb'
-    run_new_ml_int(raw_pdf_folder, working_folder, output_folder, verbosity)
+#need to adjust
+def run_new_ml_int(pdf_folder, questions, nlp_method, output_folder):
+    cmd = (
+        "python3 /app/code/new_ml_based_pipeline/new_ml_based_pipeline/main.py"
+        + ' --pdf_folder "'
+        + pdf_folder
+        + '"'
+        + ' --questions "'
+        + questions
+        + '"'
+        + " --nlp_method "
+        + str(nlp_method)
+        + ' --output_folder "'
+        + output_folder
+        + '"'
+    )
+    print("Running command: " + cmd)
+    os.system(cmd)
+
+def run_hs(project_name, Haystack, s3_usage, s3_settings):
+    base = r"/app/data/" + project_name
+    pdf_folder = base + r"/interim/pdfs/"
+    questions = base + r"/interim/newml/questions"
+    output_folder = base + r"/output/KPI_EXTRACTION/newml"
+    if s3_usage:
+        s3c_main = S3Communication(
+            s3_endpoint_url=os.getenv(s3_settings["main_bucket"]["s3_endpoint"]),
+            aws_access_key_id=os.getenv(s3_settings["main_bucket"]["s3_access_key"]),
+            aws_secret_access_key=os.getenv(s3_settings["main_bucket"]["s3_secret_key"]),
+            s3_bucket=os.getenv(s3_settings["main_bucket"]["s3_bucket_name"]),
+        )
+        create_directory(base)
+        create_directory(pdf_folder)
+        create_directory(questions)
+        create_directory(output_folder)
+        project_prefix = s3_settings["prefix"] + "/" + project_name + "/data"
+        s3c_main.download_files_in_prefix_to_dir(project_prefix + "/input/pdfs/inference", pdf_folder)
+    run_new_ml_int(pdf_folder, questions, Haystack, output_folder)
+    if s3_usage:
+        s3c_main.upload_files_in_dir_to_prefix(output_folder, project_prefix + "/output/KPI_EXTRACTION/newml")
+    return True
+
+def run_dqa(project_name, DQA, s3_usage, s3_settings):
+    base = r"/app/data/" + project_name
+    pdf_folder = base + r"/interim/pdfs/"
+    questions = base + r"/interim/newml/questions"
+    output_folder = base + r"/output/KPI_EXTRACTION/newml"
+    if s3_usage:
+        s3c_main = S3Communication(
+            s3_endpoint_url=os.getenv(s3_settings["main_bucket"]["s3_endpoint"]),
+            aws_access_key_id=os.getenv(s3_settings["main_bucket"]["s3_access_key"]),
+            aws_secret_access_key=os.getenv(s3_settings["main_bucket"]["s3_secret_key"]),
+            s3_bucket=os.getenv(s3_settings["main_bucket"]["s3_bucket_name"]),
+        )
+        create_directory(base)
+        create_directory(pdf_folder)
+        create_directory(questions)
+        create_directory(output_folder)
+        project_prefix = s3_settings["prefix"] + "/" + project_name + "/data"
+        s3c_main.download_files_in_prefix_to_dir(project_prefix + "/input/pdfs/inference", pdf_folder)
+    run_new_ml_int(pdf_folder, questions, DQA, output_folder)
+    if s3_usage:
+        s3c_main.upload_files_in_dir_to_prefix(output_folder, project_prefix + "/output/KPI_EXTRACTION/newml")
+    return True
 
 @app.route("/liveness")
 def liveness():
     return Response(response={}, status=200)
 
-
+# start point, add optional parameters
 @app.route("/run")
 def run():
-    project_name = request.args['project_name']
-    verbosity  = int(request.args['verbosity'])
-    run_new_ml(project_name, verbosity)
-    return Response(response={}, status=200)
+    try:
+        args = json.loads(request.args["payload"])
+        project_name = args["project_name"]
+        s3_settings = None
+        if args["s3_usage"]:
+            s3_settings = args["s3_settings"]
+        nlp_method = str(args["nlp_method"])
+        run_hs(project_name, Haystack, args["s3_usage"], s3_settings)
+        run_dqa(project_name, DQA, args["s3_usage"], s3_settings)
+        return Response(response={}, status=200)
+    except Exception as e:
+        m = traceback.format_exc()
+        return Response(response={m}, status=200)
 
 
 if __name__ == "__main__":
@@ -39,7 +109,7 @@ if __name__ == "__main__":
     # Add the arguments
     parser.add_argument('--port',
                         type=int,
-                        default=9000,
+                        default=8000,
                         help='port to use for the infer server')
     args = parser.parse_args()
     port = args.port
