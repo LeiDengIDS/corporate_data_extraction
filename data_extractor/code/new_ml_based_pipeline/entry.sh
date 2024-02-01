06fde784975d0cd332366cbea1779e999a51e058
LeiDengIDS <138241434+LeiDengIDS@users.noreply.github.com>
2024-02-01 11:05:54 +0100

    Update entry.sh
    Signed-off-by: LeiDengIDS <138241434+LeiDengIDS@users.noreply.github.com>

--- data_extractor/code/new_ml_based_pipeline/entry.sh
+++ data_extractor/code/new_ml_based_pipeline/entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#date_time=$(date +'%d_%h_%Y:%H_%M')
-#log_file_path="/app/server_logs/rule_based_logs_${date_time}.txt"
-#cd /app/code/rule_based_pipeline/rule_based_pipeline
-#python3 rb_server.py > $log_file_path 2>&1 &
+date_time=$(date +'%d_%h_%Y:%H_%M')
+log_file_path="/app/server_logs/new_ml_based_logs_${date_time}.txt"
+cd /app/code/new_ml_based_pipeline/new_ml_based_pipeline
+python3 new_ml_server.py > $log_file_path 2>&1 &
 
 export HOME="/app/code/new_ml_based_pipeline/myhome/"
 sleep infinity
